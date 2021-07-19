7ed9f65a871e9d98f2deee29b81f9855c545c5b4
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-07-19 12:10:32 -0500

    Added search bar on top of the nav

--- app.js
+++ app.js
@@ -20,3 +20,16 @@ function onTabClick(event) {
 const element = document.getElementById("nav-tab");
 
 element.addEventListener("click", onTabClick, false);
+
+const trigger_search = document.querySelector(".trigger_search");
+const input = document.querySelector(".input");
+
+trigger_search.addEventListener("click", () => {
+  if (!input.classList.contains("input-open")) {
+    input.classList.add("input-open");
+    trigger_search.innerHTML = "<i class='fas fa-times'>  </i>";
+  } else {
+    input.classList.remove("input-open");
+    trigger_search.innerHTML = "<i class='fas fa-search'>  </i>";
+  }
+});
